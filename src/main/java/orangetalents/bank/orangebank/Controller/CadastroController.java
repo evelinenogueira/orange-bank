2ae429219f0c2f089436d22be535a530f34370bf
evelinenogueira <evelinecontato@gmail.com>
2021-01-09 00:11:01 -0300

    conclusão

--- src/main/java/orangetalents/bank/orangebank/Controller/CadastroController.java
+++ src/main/java/orangetalents/bank/orangebank/Controller/CadastroController.java
@@ -1,15 +1,12 @@
 package orangetalents.bank.orangebank.Controller;
 
-import orangetalents.bank.orangebank.DTO.CadastroDTO;
 import orangetalents.bank.orangebank.Domain.Cadastro;
 import orangetalents.bank.orangebank.Service.CadastroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,29 +17,26 @@ public class CadastroController {
     CadastroService cadastroService;
 
 
- @GetMapping("/{id}")
- public ResponseEntity<Optional<Cadastro>> Cadastro (@Valid @PathVariable Integer id){
-    try {
-        return new ResponseEntity<>(cadastroService.findById(id), HttpStatus.OK);
-    }catch (NullPointerException e){
+    @PostMapping
+    @RequestMapping("/novo")
+    public ResponseEntity<Cadastro> save(@Valid @RequestBody Cadastro cadastro) {
 
-        return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        try {
+            return new ResponseEntity<>(cadastroService.save(cadastro), HttpStatus.CREATED);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
     }
- }
- @PostMapping
 
- public ResponseEntity<Cadastro> save(@Valid @RequestBody Cadastro cadastro){
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Cadastro>> Cadastro(@Valid @PathVariable Integer id) {
+        try {
+            return new ResponseEntity<>(cadastroService.findById(id), HttpStatus.OK);
+        } catch (NullPointerException e) {
 
-     try{
-         return new ResponseEntity<>(cadastroService.save(cadastro),HttpStatus.CREATED);
-
-     }catch (Exception e){
-         return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-     }
- }
-    @GetMapping
-    public List<CadastroDTO> findAll() {
-
-        return CadastroDTO.convert(cadastroService.findAll());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/orangetalents/bank/orangebank/Repository/CadastroRepository.java
+++ src/main/java/orangetalents/bank/orangebank/Repository/CadastroRepository.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface CadastroRepository extends JpaRepository<Cadastro, Integer> {
 
 
-
-
 }
